87d9ed7d38d8ce161dca6b33a7007ff398d2550e
Andrei Riaskov <code@riaskov.com>
2020-05-18 08:50:06 +0400

    Bring account-link and tile ceramic handlers & its tests to potter.

---
+++ apps/wheel/src/lib/ceramic/handlers/account-link.handler.ts
@@ -0,0 +1,40 @@
+import CID from 'cids'
+import { Doctype } from '../doctype'
+import { DocumentState } from '../document.state'
+import { IHandler } from './handler.interface'
+
+export class AccountLinkHandler implements IHandler {
+  static DOCTYPE = Doctype.ACCOUNT_LINK
+
+  async makeGenesis (record: any): Promise<any> {
+    if (!record.owners) throw new Error('Owner must be specified')
+    if (record.owners.length !== 1) throw new Error('Exactly one owner must be specified')
+    return {
+      ...record,
+      doctype: 'account-link'
+    }
+  }
+
+  async applyGenesis (record: any, cid: CID): Promise<DocumentState> {
+    // TODO - verify genesis record
+    return {
+      doctype: AccountLinkHandler.DOCTYPE,
+      owners: record.owners,
+      content: null,
+      nextContent: null,
+      log: [cid]
+    }
+  }
+
+  async makeRecord (state: DocumentState, proof: any): Promise<any> {
+    const record = { content: proof, prev: state.log[state.log.length - 1] }
+    return record
+  }
+
+  async applySigned (record: any, cid: CID, state: DocumentState): Promise<DocumentState> {
+    state.log.push(cid)
+    return {
+      ...state
+    }
+  }
+}
--- apps/wheel/src/lib/ceramic/handlers/handler.interface.ts
+++ apps/wheel/src/lib/ceramic/handlers/handler.interface.ts
@@ -4,6 +4,6 @@ import { DocumentState } from '../document.state';
 export interface IHandler {
   applyGenesis(record: any, cid: CID): Promise<any>
   makeGenesis(record: any): Promise<any>
-  makeRecord(state: DocumentState, next: any): Promise<any>
+  makeRecord(state: DocumentState, nextOrProof: any): Promise<any>
   applySigned (record: any, cid: CID, state: DocumentState): Promise<DocumentState>;
 }
