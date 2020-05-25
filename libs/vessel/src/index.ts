8e88f7f09f91da1e96adb2c442e2160615f946fe
ukstv <sergey@ukstv.me>
2020-05-25 18:15:50 +0400

    Move ledger to vessel

--- libs/vessel/src/index.ts
+++ libs/vessel/src/index.ts
@@ -1,2 +1,4 @@
-export * from "./three-id";
-export * from './ceramic-document-id'
+export * from './three-id';
+export * from './ceramic-document-id';
+export * from './chain';
+export * from './chain-entry';
