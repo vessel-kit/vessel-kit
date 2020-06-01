c3b4270259cac58f8460f6932f7143c96ede1a31
ukstv <sergey@ukstv.me>
2020-06-01 21:51:35 +0400

    WIP

--- libs/vessel/src/integration/post-document.ts
+++ libs/vessel/src/integration/post-document.ts
@@ -6,22 +6,35 @@ import { AnchoringStatus } from '..';
 import { KeyPrecious } from '../person/key-precious';
 import IdentityWallet from 'identity-wallet';
 import { LocalUser } from '../person/local.user';
+import { Signor } from '../person/signor';
 
 const IPFS_URL = 'http://localhost:5001';
 const ipfs = ipfsClient(IPFS_URL);
+
+const cborSortCompareFn = (a: string, b: string): number => a.length - b.length || a.localeCompare(b)
+
+function sortPropertiesDeep(obj: any, compareFn: (a: any, b: any) => number = cborSortCompareFn): any {
+  if (typeof obj !== 'object' || Array.isArray(obj)) {
+    return obj
+  }
+  return Object.keys(obj).sort(compareFn).reduce<Record<string, any>>((acc, prop) => {
+    acc[prop] = sortPropertiesDeep(obj[prop], compareFn)
+    return acc
+  }, {})
+}
 
 async function main() {
   const identityWallet = new IdentityWallet(() => true, {
     seed: '0xf533035c3339782eb95ffdfb7f485ac2c74545033a7cb2a46b6c91f77ae33b8f',
   });
-  const localUser = await LocalUser.fromIdentityWallet(identityWallet);
-  return
+  const user = new Signor(identityWallet.get3idProvider());
+  await user.auth();
 
   const ceramic = await Ceramic.build(ipfs);
 
-  const ownerKey = await localUser.keyPrecious.managementKey();
-  const signingKey = await localUser.keyPrecious.signingKey();
-  const encryptionKey = await localUser.keyPrecious.asymEncryptionKey();
+  const ownerKey = user.publicKeys.managementKey;
+  const signingKey = user.publicKeys.signingKey;
+  const encryptionKey = user.publicKeys.asymEncryptionKey;
 
   const doc1 = new ThreeIdContent(
     [ownerKey],
@@ -49,12 +62,24 @@ async function main() {
   const doc2 = doc1.clone();
   doc2.publicKeys.set('foocryption', signingKey);
   const delta = doc2.delta(doc1);
-  console.log('delta', delta);
   const updateRecord = {
     patch: delta,
     prev: document.state.log.last,
+    id: document.id.cid,
   };
-  localUser.signManagement(updateRecord);
+  const updateRecordToSign = sortPropertiesDeep({
+    patch: delta,
+    prev: { '/': document.state.log.last.toString() },
+    id: { '/': document.id.valueOf() },
+  })
+  user.did = `did:3:${document.id.valueOf()}`
+  const a = await user.sign(updateRecordToSign, { useMgmt: true });
+  const updateRecordA = {
+    ...updateRecord,
+    header: a.header,
+    signature: a.signature
+  }
+  // localUser.signManagement(updateRecord);
 }
 
 main();
