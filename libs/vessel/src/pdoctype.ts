540f1e8f022808b13d66d0c87d4a24ebafb0d708
ukstv <sergey@ukstv.me>
2020-07-23 19:17:23 +0400

    Exterminatus

--- libs/vessel/src/pdoctype.ts
+++
@@ -1,8 +0,0 @@
-import { IWithDoctype } from './document/with-doctype.interface';
-import { IWIthRawHistory } from './document/reducible';
-import { IContext } from './context';
-
-export interface PDoctype<A extends IWithDoctype & IWIthRawHistory = IWithDoctype & IWIthRawHistory> {
-  readonly context: IContext
-  create(genesisRecord: unknown): Promise<A>
-}
