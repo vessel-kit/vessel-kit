fea9e03eef64797c3f90d0d9956235944c4a15b8
Andrei Riaskov <code@riaskov.com>
2020-09-02 11:21:14 +0300

    Add OpenAPI docs.

--- apps/anchoring-api/src/api/request.presentation.ts
+++ apps/anchoring-api/src/api/request.presentation.ts
@@ -1,12 +1,19 @@
 import { RequestRecord } from '../storage/request.record';
 import { AnchorRecord } from '../storage/anchor.record';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RequestPresentation {
+  @ApiProperty({ example: '3ce7f07f-284e-405e-8833-d05494329d1d', description: 'Anchor ID' })
   readonly id = this.request.id.toString();
+  @ApiProperty({ example: 'ANCHORED', description: 'Anchoring request status' })
   readonly status = this.request.status.toString();
+  @ApiProperty({ example: 'ceramic://bafyreidygxyu2ohh3cuuj6lwm3hnb6u5ruy43pzc2syc7mztuu5zuemfk4', description: 'Ceramic Document ID' })
   readonly docId = this.request.docId;
+  @ApiProperty({ example: '2020-08-21 20:37:17.881', description: 'Anchoring request creation date' })
   readonly createdAt = this.request.createdAt.toISOString();
+  @ApiProperty({ example: '2020-08-25 15:33:29.105', description: 'Anchoring request update date' })
   readonly updatedAt = this.request.updatedAt.toISOString();
+  @ApiProperty({ example: 'bafyreieoftma6uomy7au4cyptcsndxi7xni5owzax4tetxznt74brpmrdy', description: 'CID' })
   readonly cid = this.request.cid.toString();
 
   constructor(
--- apps/anchoring-api/src/api/stats.controller.ts
+++ apps/anchoring-api/src/api/stats.controller.ts
@@ -3,6 +3,11 @@ import { RequestStorage } from '../storage/request.storage';
 import { AnchorStorage } from '../storage/anchor.storage';
 import { AnchoringScheduleService } from '../anchoring/anchoring-schedule.service';
 import { AnchoringStatus } from '@potter/anchoring';
+import {
+  ApiOperation,
+  ApiTags,
+  ApiResponse
+} from '@nestjs/swagger';
 
 @Controller('/v0/stats')
 export class StatsController {
@@ -13,6 +18,11 @@ export class StatsController {
   ) {}
 
   @Get('/')
+  @ApiTags('stats')
+  @ApiOperation({ summary: 'Get stats', description: 'Gather statistics about total amount of requests, ' +
+      'total amount of anchors, pending requests, next anchoring time'})
+  @ApiResponse({ status: 200, description: 'Success'})
+  @ApiResponse({ status: 500, description: 'Error'})
   async index() {
     const requestsTotalCount = await this.requestStorage.count();
     const anchorsTotalCount = await this.anchorStorage.count();
