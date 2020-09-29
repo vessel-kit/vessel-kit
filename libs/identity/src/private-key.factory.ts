0f0812e4000cb162938e26015c81f721cd63c84a
ukstv <sergey@ukstv.me>
2020-09-29 23:34:00 +0400

    ♻️️ [identity] Rename internal vars in PrivateKeyFactory

--- libs/identity/src/private-key.factory.ts
+++ libs/identity/src/private-key.factory.ts
@@ -1,23 +1,23 @@
 import { AlgorithmKind } from './algorithm-kind';
 import * as sha256 from '@stablelib/sha256';
 import { InvalidAlgorithmKindError } from './invalid-algorithm-kind.error';
-import * as secp256k1 from './algorithms/ES256K';
-import * as ed25519 from './algorithms/EdDSA';
+import * as ES256K from './algorithms/ES256K';
+import * as EdDSA from './algorithms/EdDSA';
 import { IPrivateKey } from './private-key.interface';
 
 const encoder = new TextEncoder();
 
 export class PrivateKeyFactory {
-  fromSeed(kind: AlgorithmKind.ES256K, seed: Uint8Array | string): secp256k1.PrivateKey;
-  fromSeed(kind: AlgorithmKind.EdDSA, seed: Uint8Array | string): ed25519.PrivateKey;
+  fromSeed(kind: AlgorithmKind.ES256K, seed: Uint8Array | string): ES256K.PrivateKey;
+  fromSeed(kind: AlgorithmKind.EdDSA, seed: Uint8Array | string): EdDSA.PrivateKey;
   fromSeed(kind: AlgorithmKind, seed: Uint8Array | string): IPrivateKey {
     const bytes = typeof seed === 'string' ? encoder.encode(seed) : seed;
     const material = sha256.hash(bytes);
     switch (kind) {
       case AlgorithmKind.EdDSA:
-        return new ed25519.PrivateKey(material);
+        return new EdDSA.PrivateKey(material);
       case AlgorithmKind.ES256K:
-        return new secp256k1.PrivateKey(material);
+        return new ES256K.PrivateKey(material);
       /* istanbul ignore next */
       default:
         throw new InvalidAlgorithmKindError(kind);
